299964e845461afc390b97afcc1336a68d842374
Akku_1498 <akash.singh@worldline.com>
2026-07-31 20:36:20 +0530

    Adding project to repository

---
+++ src/main/java/org/hx/ci/hxapplication/HxApplication.java
@@ -0,0 +1,13 @@
+package org.hx.ci.hxapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hx/ci/hxapplication/HxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hx.ci.hxapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
